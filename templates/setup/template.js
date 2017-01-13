3e11ef91957e9a921c04b2a6872c8c76fd6a8d21
Cole R Lawrence <msgzht@gmail.com>
2017-01-13 03:17:09 -0600

    Add setup template

---
+++ templates/setup/template.js
@@ -0,0 +1,19 @@
+/**
+ * This template is for setting up the secrets files.
+ * You may also use the *.example files to guide you,
+ * but configuring all the files through this may be
+ * the easier way to go.
+ *
+ * just `npm i -g undergen`
+ * and run `under gen setup`
+ */
+
+module.exports = {
+  variables: [
+    { name: "APP_ID", default: () => "com.garstapp.v1" },
+    { name: "APP_NAME", default: () => "Garst" },
+    { name: "APP_VERSION", default: () => "1.0.0" },
+    { name: "FACEBOOK_APP_ID" },
+    { name: "FACEBOOK_APP_NAME" },
+  ]
+}
